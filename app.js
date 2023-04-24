4bd6260dc8f18c20cc8724d5de36dea5c617796d
JJoseph1337 <115859098+JJoseph1337@users.noreply.github.com>
2023-04-24 11:12:14 +0400

    Add files via upload

---
+++ app.js
@@ -0,0 +1,30 @@
+const menuBurger = document.querySelector('.menuIcon');
+const menu = document.querySelector(".menu");
+const menuProject = document.querySelector(".menuList__item_project");
+const subMenuProject = document.querySelector(".subMenu-project");
+const burgerIcon = document.querySelector(".burgerIcon");
+const body = document.querySelector(".body");
+
+menuBurger.onclick = () => {
+    menu.classList.toggle("menu-active");
+
+    if (menu.classList.contains("menu-active")) {
+        burgerIcon.src = "/images/icon-close.svg";
+
+    } else {
+        burgerIcon.src = "/images/icon-hamburger.svg";
+    }
+}
+
+menuProject.onclick = () => {
+    subMenuProject.classList.toggle("subMenu-active");
+}
+
+
+document.onclick = (e) => {
+    if (!menu.contains(e.target) && !menuBurger.contains(e.target)) {
+        menu.classList.remove('menu-active');
+        burgerIcon.src = "/images/icon-hamburger.svg";
+        console.log('done')
+    }
+}
